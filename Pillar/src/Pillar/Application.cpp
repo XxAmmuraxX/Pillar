e11102fd668b005deb155966aca913d83b7b2da3
XxAmmuraxX <omarakkawi10@gmail.com>
2025-10-14 17:13:31 +0200

    Linked the windows events to the pillar events

--- Pillar/src/Pillar/Application.cpp
+++ Pillar/src/Pillar/Application.cpp
@@ -2,35 +2,52 @@
 #include "stdio.h"
 #include "Application.h"
 #include "Pillar/Logger.h"
-#include "Pillar/Events/ApplicationEvent.h"
+
 
 namespace Pillar
 {
+
+#define BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create(WindowProps("Pillar Engine", 1280, 720)));
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 
 	}
 	Application::~Application()
 	{
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
+		PIL_CORE_TRACE("{0}", e.ToString());
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
+
+
 	void Application::Run()
 	{
 
 		printf("Pillar Engine is running...\n");
+		PIL_CORE_INFO("Application is running...");
+		PIL_CORE_ERROR("This is an error message for demonstration purposes.");
+		PIL_CORE_WARN("This is a warning message for demonstration purposes.");
 		while (m_Running)
 		{// Simulate application running
 			m_Window->OnUpdate();
-			PIL_CORE_INFO("Application is running...");
-			PIL_CORE_ERROR("This is an error message for demonstration purposes.");
-			PIL_CORE_WARN("This is a warning message for demonstration purposes.");
+
 			// Here you would typically handle events, update the application state, render, etc.
 			// For demonstration purposes, we will just sleep for a while.
-			std::this_thread::sleep_for(std::chrono::seconds(1));
-			// Simulate an event
-			WindowResizeEvent resizeEvent(1280, 720);
-			PIL_CORE_TRACE("Event: {0}", resizeEvent.ToString());
+			//std::this_thread::sleep_for(std::chrono::seconds(1));
+
 		}
 	}
 }
